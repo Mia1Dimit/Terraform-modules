1dd6af6087ce1a299c5223d1960de821dd431b78
Dimitris Miaoulis <jimiaoul@gmail.com>
2026-06-09 17:36:54 +0200

    refactor(cognito-user-pool): align files with resource name updates

--- cognito-user-pool/main.tf
+++ cognito-user-pool/main.tf
@@ -1,11 +1,11 @@
-resource "aws_cognito_user_pool" "this" {
+resource "aws_cognito_user_pool" "cognito_user_pool" {
   name = var.pool_name
   tags = local.merged_tags
 }
 
-resource "aws_cognito_user_pool_client" "this" {
+resource "aws_cognito_user_pool_client" "cognito_user_pool_client" {
   name                                 = var.app_client_name
-  user_pool_id                         = aws_cognito_user_pool.this.id
+  user_pool_id                         = aws_cognito_user_pool.cognito_user_pool.id
   generate_secret                      = false
   allowed_oauth_flows_user_pool_client = true
   allowed_oauth_flows                  = var.allowed_oauth_flows
@@ -15,8 +15,8 @@ resource "aws_cognito_user_pool_client" "this" {
   supported_identity_providers         = ["COGNITO"]
 }
 
-resource "aws_cognito_user_pool_domain" "this" {
+resource "aws_cognito_user_pool_domain" "cognito_user_pool_domain" {
   count        = var.domain_prefix != "" ? 1 : 0
   domain       = var.domain_prefix
-  user_pool_id = aws_cognito_user_pool.this.id
+  user_pool_id = aws_cognito_user_pool.cognito_user_pool.id
 }
