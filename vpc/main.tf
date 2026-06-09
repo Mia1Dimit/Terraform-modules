0a66c51f320f04725976fafaee31646ab66cb0c2
Dimitris Miaoulis <jimiaoul@gmail.com>
2026-06-09 17:36:56 +0200

    refactor(vpc): align files with resource name updates

--- vpc/main.tf
+++ vpc/main.tf
@@ -4,30 +4,30 @@ data "aws_route_table" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_vpc" "this" {
+resource "aws_vpc" "vpc" {
   cidr_block = var.cidr_block
   tags       = merge(local.merged_tags, { Name = var.name })
 }
 
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.this.id
+  vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public_subnet_cidr
   availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
   tags                    = merge(local.merged_tags, { Name = "${var.name}-public" })
 }
 
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
   tags   = merge(local.merged_tags, { Name = "${var.name}-igw" })
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.this.id
+  vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
   tags = merge(local.merged_tags, { Name = "${var.name}-public-rt" })
@@ -41,7 +41,7 @@ resource "aws_route_table_association" "public" {
 resource "aws_vpc_endpoint" "gateway" {
   for_each = toset(var.endpoint_services)
 
-  vpc_id            = aws_vpc.this.id
+  vpc_id            = aws_vpc.vpc.id
   service_name      = "com.amazonaws.${data.aws_region.current.name}.${each.key}"
   vpc_endpoint_type = "Gateway"
   route_table_ids   = [aws_route_table.public.id]
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,4 +1,4 @@
-output "vpc_id" { value = aws_vpc.this.id }
+output "vpc_id" { value = aws_vpc.vpc.id }
 output "public_subnet_id" { value = aws_subnet.public.id }
 output "route_table_id" { value = aws_route_table.public.id }
 output "vpc_endpoint_ids" { value = { for k, v in aws_vpc_endpoint.gateway : k => v.id } }
