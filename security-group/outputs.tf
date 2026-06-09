31633483ebca18b5498a28064c1d99786cae1ce2
Dimitris Miaoulis <jimiaoul@gmail.com>
2026-06-09 17:36:55 +0200

    refactor(security-group): align files with resource name updates

--- security-group/outputs.tf
+++ security-group/outputs.tf
@@ -1,2 +1,2 @@
-output "security_group_id" { value = aws_security_group.this.id }
-output "security_group_arn" { value = aws_security_group.this.arn }
+output "security_group_id" { value = aws_security_group.security_group.id }
+output "security_group_arn" { value = aws_security_group.security_group.arn }
